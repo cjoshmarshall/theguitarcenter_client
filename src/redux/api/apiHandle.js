83d82ef921c2db5dc4d927bc881506e703eaf2e3
cjoshmarshall <cjoshmarshall@gmail.com>
2022-04-29 21:20:52 +0530

    fix 1.1

--- src/redux/api/apiHandle.js
+++ src/redux/api/apiHandle.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL="http://localhost:3006/api"
-const BASE_URL="https://theguitarcenter.heroku.app/api"
+const BASE_URL="https://theguitarcenter.herokuapp.com/api"
 const TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjE4ZTVmYzRiZDZkOTc4MWYyY2E5NSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NTExMzEyODUsImV4cCI6MTY1MTM5MDQ4NX0.3swnk-FzEHfXDct1Kut9HQZdKL_-wI-hOzA2DRg6wU8"
 
 export const publicRequest=axios.create({
